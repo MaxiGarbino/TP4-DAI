e3b0f1751590d7c44b4391a7543bf6fe2c69c602
MAXIMILIANO GARBARINO <47433817@ort>
2024-04-26 11:28:05 -0300

    finalizado

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ app.get("/api/province", (req, res) => {
 
 app.get("/api/province/:id", (req,res)=>{
     const id = req.params.id;
-    const seEncontroProvincia  = provincias.find(province => province.id == id);
-    if (seEncontroProvincia ) {
-        res.send(province);
+    const index = provincias.findIndex(provincia => provincia.id == id);
+    if (index !== -1) {
+        res.send(provincias[index]);
     } else {
         res.status(404).send({ error: "Provincia no encontrada" });
     }
@@ -75,9 +75,9 @@ app.delete("/api/province/:id", (req, res) => {
     const index = provincias.findIndex(provincia => provincia.id == id);
     if (index !== -1) {
         provincias.splice(index, 1);
-        res.status(200).json({ mensaje: "Provincia eliminada correctamente" });
+        res.status(200).send({ mensaje: "Provincia eliminada correctamente" });
     } else {
-        res.status(404).json({ error: "Provincia no encontrada" });
+        res.status(404).send({ error: "Provincia no encontrada" });
     }
 });
 
