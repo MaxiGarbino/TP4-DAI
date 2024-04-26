67bd1abf7a7e0ca20c623e2c130e9af9c9db8181
MAXIMILIANO GARBARINO <47433817@ort>
2024-04-26 11:23:12 -0300

    Post completado

--- index.js
+++ index.js
@@ -22,7 +22,28 @@ app.get("/api/province/:id", (req,res)=>{
         res.status(404).send({ error: "Provincia no encontrada" });
     }
 })
-
+app.post('/api/province',async(req,res)=>{
+    try{
+        let nombre = req.body.name;
+        let full_nombre = req.body.full_name;
+        let latitud = parseInt(req.body.latitude);
+        let longitud = parseInt(req.body.longitude);
+        let display_orden = parseInt(req.body.display_order);
+        provincias.push({
+        id: (provincias.length+1),
+        name: nombre,
+        full_name: full_nombre,
+        latitude: latitud,
+        longitude:longitud,
+        display_order:display_orden
+    });
+    res.status(201).send("created");
+    } catch(error){
+        console.log(error)
+        res.status(400).send(error.message);
+    }
+    
+})
 app.delete("/api/province/:id", (req, res) => {
     const id = req.params.id;
     const index = provincias.findIndex(provincia => provincia.id == id);
