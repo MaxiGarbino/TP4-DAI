5fc6ec423d52600b476ba9ac5abf495b001a7ed6
MAXIMILIANO GARBARINO <47433817@ort>
2024-04-26 08:54:54 -0300

    push array alumnos

---
+++ src/controllers/province-controller.js
@@ -0,0 +1,39 @@
+const provincias = [
+    {
+      name: "Chaco Provincia",
+      full_name: "Provincia de Chaco",
+      latitude: -24.895086288452148,
+      longitude: -59.93218994140625,
+      display_order: 100
+    },
+    {
+      name: "Buenos Aires Provincia",
+      full_name: "Provincia de Buenos Aires",
+      latitude: -36.6769,
+      longitude: -60.5586,
+      display_order: 101
+    },
+    {
+      name: "Córdoba Provincia",
+      full_name: "Provincia de Córdoba",
+      latitude: -32.1429,
+      longitude: -63.8018,
+      display_order: 102
+    },
+    {
+      name: "Santa Fe Provincia",
+      full_name: "Provincia de Santa Fe",
+      latitude: -31.6107,
+      longitude: -60.6973,
+      display_order: 103
+    },
+    {
+      name: "Mendoza Provincia",
+      full_name: "Provincia de Mendoza",
+      latitude: -32.8895,
+      longitude: -68.8458,
+      display_order: 104
+    }
+  ];
+  
+export {provincias};
