fd4a780d2481c82d7a79156b1eeb31f37b9be9d5
MAXIMILIANO GARBARINO <47433817@ort>
2024-04-26 09:42:02 -0300

    solucionando error correr archivo

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 import express from "express";
 import cors from "cors";
-import ProvinceRouter from "./src/controllers/province-controller.js"
-import {provincias} from "./src/controllers/province-controller.js"
+import { getAllProvinces, getProvinceById } from "./src/controllers/province-controller.js";
 
 const app = express();
-const port = 3000; // El puerto 3000 (http://localhost:3000)
-// Agrego los Middlewares
-app.use(cors()); // Middleware de CORS.
-app.use(express.json()); // Middleware para parsear y comprender JSON.
-//
-// Endpoints (todos los Routers)
-//
-app.use("/api/province", ProvinceRouter);
-//
-// Inicio el Server y lo pongo a escuchar.
-//
+const port = 3000;
+app.use(cors()); 
+app.use(express.json()); 
+
+
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/api/province', function (req, res) {
-    
-    res.send(provincias)
-});
-app.get('/api/province', function (req, res) {
-    
-    res.send()
-});
+app.get("/api/province", getAllProvinces);
+
+app.get("/api/province/:id", getProvinceById);
--- src/controllers/province-controller.js
+++ src/controllers/province-controller.js
@@ -1,53 +1,16 @@
-const provincias = [
-    {
-      id: 1,
-      name: "Chaco Provincia",
-      full_name: "Provincia de Chaco",
-      latitude: -24.895086288452148,
-      longitude: -59.93218994140625,
-      display_order: 100
-    },
-    {
-      id: 2,
-      name: "Buenos Aires Provincia",
-      full_name: "Provincia de Buenos Aires",
-      latitude: -36.6769,
-      longitude: -60.5586,
-      display_order: 101
-    },
-    {
-      id: 3,
-      name: "Córdoba Provincia",
-      full_name: "Provincia de Córdoba",
-      latitude: -32.1429,
-      longitude: -63.8018,
-      display_order: 102
-    },
-    {
-      id: 4,
-      name: "Santa Fe Provincia",
-      full_name: "Provincia de Santa Fe",
-      latitude: -31.6107,
-      longitude: -60.6973,
-      display_order: 103
-    },
-    {
-      id: 5,
-      name: "Mendoza Provincia",
-      full_name: "Provincia de Mendoza",
-      latitude: -32.8895,
-      longitude: -68.8458,
-      display_order: 104
-    }
-  ];
-  
-  console.log(provincias);
-  
-export {provincias};
+import {provincias} from "../entities/province.js"
 
-const ProvinceController = {
-    // ... tu código aquí
-  };
-  
-  export default ProvinceController;
-  
+export const getAllProvinces = (req, res) => {
+    res.json(provincias);
+};
+
+export const getProvinceById = (req, res) => {
+    const id = req.params.id;
+    const province = provincias.find(province => province.id === id);
+    if (province) {
+        res.json(province);
+    } else {
+        res.status(404).json({ error: "Provincia no encontrada" });
+    }
+};
+
