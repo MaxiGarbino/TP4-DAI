e7890098ca23ad9b7e8be9ecb5b7975719f74624
RAIAN JU HO KIM <47963062@ort>
2024-04-26 11:20:37 -0300

    .

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
 })
 
+app.get("/api/province", (req, res) => {
+    res.send(provincias);
+});
 
 app.get("/api/province/:id", (req,res)=>{
     const id = req.params.id;
@@ -21,6 +24,28 @@ app.get("/api/province/:id", (req,res)=>{
     } else {
         res.status(404).send({ error: "Provincia no encontrada" });
     }
+})
+
+app.put("/api/province", async(req,res) =>
+{
+    const id = req.body.id;
+    console.log(req.body.name)
+    const provincefv = provincias.find(province => province.id == id);
+    if (provincefv) {
+        if( req.body.name="" || req.body.name.length <= 3)
+        {
+            res.status(400).send({error: "No cumple con las reglas de negocio"})
+        }
+        else
+        {
+            res.status(201).send("Created")
+            provincias[id-1] = {id:id,name:req.body.name,full_name : req.body.full_name,latitude: req.body.latitude,longitude: req.body.longitude,display_order : req.body.display_order}
+        }
+    } 
+    else {
+        res.status(404).send({ error: "Provincia no encontrada" });
+    }
+
 })
 
 app.delete("/api/province/:id", (req, res) => {
